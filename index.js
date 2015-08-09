691135370b967f52c85bfab48dbb48e0f5957f02
Jorge Vivas <slacksky@gmail.com>
2015-08-09 18:08:47 -0430

    readding from index.html

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+var fs = require('fs');
 var express = require('express')
 var app = express()
-
+//buf = new Buffer(27);
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+
+//fs.readFile('index.html', function (err, data) {
+//  if (err) throw err;
+//  console.log(data.toString('utf8',0,27));
+//});
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+     response.send(data.toString('utf8',0,27))
+  //console.log(data.toString('utf8',0,27));
+  });
+  //response.send(data.toString('utf8',0,27))
 })
 
 app.listen(app.get('port'), function() {
